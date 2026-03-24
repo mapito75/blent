f9ce56a3e2b095ce480a812c004a093b4273f994
Marie pierre Towlson <mapito@MacBook-Pro-de-Marie-2.local>
2026-03-24 12:39:43 +0100

    Add ORM module and test4 script

---
+++ test4.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def print_response(response):
+    print(f"Status: {response.status_code}")
+    print("Response:", json.dumps(response.json(), indent=2))
+
+
+rep = requests.post("http://127.0.0.1:5000/api/auth/register", json={ "email": "jean@example.com", "password": "motdepasse123",
+  "first_name": "Jean", "last_name": "Dupont", "role": "client" }
+)
+
+
+rep = requests.post("http://127.0.0.1:5000/api/auth/login", json={ "email": "jean@example.com", "password": "motdepasse123" })
+my_token = json.loads(rep.content)["token"]
+print(rep.status_code, json.loads(rep.content))
+print("my_token", my_token)
+
